7b8de828d2978071deb3fe8a999a97b816df8b05
Don Sirivat <don@joinhandshake.com>
2020-04-29 07:42:41 -0500

    Add initial styling to home component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -10,18 +10,22 @@ import CheckinListContainer from './checkin_list/checkin_list_container';
 
 const App = () => (
   <div>
-    <header>
-      <h1>Handshake Check-in Kiosk</h1>
+    <div className="home-container">
+      <div className="home-image-container">
+        <img className="handshake-logo-icon" src="https://education.jhu.edu/wp-content/uploads/2018/11/Handshake-logo.png" />
+      </div>
+      <div className="home-text-container">
+        <h1>Handshake Check-in Kiosk</h1>
+        <Link to="/checkins/new" className="header-link">
+          <h2>New Check-in</h2>
+        </Link>
+        <Link to="/checkins" className="header-link">
+          <h2>Check-in List</h2>
+        </Link>
+      </div>
+    </div>
 
-      <Link to="/checkins/new" className="header-link">
-        <h2>New Check-in</h2>
-      </Link>
-      <Link to="/checkins" className="header-link">
-        <h2>Check-in List</h2>
-      </Link>
-    </header>
     <Switch>
-      
       <Route path="/checkins/new" component={CheckinFormContainer} />
       <Route exact path="/checkins" component={CheckinListContainer} />
     </Switch>
